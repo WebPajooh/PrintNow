decdad8163fe919e7face11ee2dd036201f4cc33
WebPajooh <mohammadmp7676@gmail.com>
2019-06-08 16:29:11 +0430

    Fix a problem

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
 
 define('PLUGIN_NAME', trim(dirname(plugin_basename(__FILE__)), '/'));
-define('PLUGIN_DIR', WP_PLUGIN_DIR . '/' . MYPLUGIN_PLUGIN_NAME);
-define('PLUGIN_URL', WP_PLUGIN_URL . '/' . MYPLUGIN_PLUGIN_NAME);
+define('PLUGIN_DIR', WP_PLUGIN_DIR . '/' . PLUGIN_NAME);
+define('PLUGIN_URL', WP_PLUGIN_URL . '/' . PLUGIN_NAME);

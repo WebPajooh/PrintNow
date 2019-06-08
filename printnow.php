2d6824a3541c82f54f19aa98b010edb8d002fca0
WebPajooh <mohammadmp7676@gmail.com>
2019-06-08 16:27:45 +0430

    Add config.php

--- printnow.php
+++ printnow.php
@@ -7,6 +7,8 @@ Author: WebPajooh
 Author URI: http://webpajooh.github.io/WebPajooh
 */
 
+require 'includes/config.php';
 require 'includes/Install.php';
+require 'includes/PrintPage.php';
 
 add_action('activated_plugin', 'Install::createPrintPage');

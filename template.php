a9800311d674d8a34cdc7c89ebe189f6589c2fb9
WebPajooh <mohammadmp7676@gmail.com>
2019-06-09 10:04:49 +0430

    Update template.php

--- template.php
+++ template.php
@@ -1,3 +1,27 @@
 <?php
 
-// TODO
+$postID = get_query_var('post_id');
+$post = get_post($postID);
+
+if (! $post) {
+	wp_redirect(home_url(), 301);
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $post->post_title; ?> - نسخه چاپی</title>
+</head>
+<body>
+	<div class="container">
+		<article id="post">
+			<h1><?php echo $post->post_title; ?></h1>
+			<div class="postContent">
+				<?php echo $post->post_content; ?>
+			</div>
+		</article>
+	</div>
+</body>
+</html>

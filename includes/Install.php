97bc0e1adda1b1f42d88ffa59037c4d9bf22d4e3
WebPajooh <mohammadmp7676@gmail.com>
2019-06-09 09:33:49 +0430

    Dont create the page if exist

--- includes/Install.php
+++ includes/Install.php
@@ -4,13 +4,15 @@ class Install
 {
 	public static function createPrintPage()
 	{
-		$page = [
-			'post_type'   => 'page',
-			'post_title'  => 'نسخه چاپی',
-			'post_status' => 'publish',
-			'post_author' => 1,
-			'post_name'   => 'print'
-		];
-		wp_insert_post($page);
+		if (!get_page_by_path('print')) {
+			$page = [
+				'post_type'   => 'page',
+				'post_title'  => 'نسخه چاپی',
+				'post_status' => 'publish',
+				'post_author' => 1,
+				'post_name'   => 'print'
+			];
+			wp_insert_post($page);
+		}
 	}
 }

a6208284796987dddd0a8165269e3d514fb90c7e
WebPajooh <mohammadmp7676@gmail.com>
2019-06-10 17:14:05 +0430

    Add PrintWidget class

---
+++ includes/PrintWidget.php
@@ -0,0 +1,36 @@
+<?php
+
+class PrintWidget extends WP_Widget
+{
+	function __construct()
+	{
+		parent::__construct(false, 'PrintNow');
+	}
+
+	function widget($args, $instance)
+	{
+		if (! is_single() && ! is_page()) {
+			return;
+		}
+
+		$printLink = "'" . get_bloginfo('home') . '/print?post_id=' . get_the_id() . "'";
+
+		echo '<div id="sidebarPrintNowWidget">
+			<button onclick="window.open(' . $printLink . ')">Print!</button>
+		</div>';
+	}
+
+	function update($new_instance, $old_instance)
+	{
+		// Save widget options
+	}
+
+	// function form($instance)
+	// {
+	// 	//
+	// }
+}
+
+function registerPrintWidget() {
+	register_widget('PrintWidget');
+}
--- printnow.php
+++ printnow.php
@@ -10,8 +10,10 @@ Author URI: http://webpajooh.github.io/WebPajooh
 require 'config.php';
 require 'includes/helpers.php';
 require 'includes/Plugin.php';
+require 'includes/PrintWidget.php';
 require 'includes/PrintPage.php';
 
 add_action('activated_plugin', 'Plugin::createPrintPage');
 add_filter('query_vars', 'Plugin::addPostIDVar');
+add_filter('widgets_init', 'registerPrintWidget');
 add_filter('page_template', 'PrintPage::setTheTemplate');
